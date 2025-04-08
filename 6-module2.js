a267f9ef8a8a1dc0ab32f0ae453873c5d28d2ebc
amitpoitrix <amit.poitrix@gmail.com>
2025-04-08 10:08:22 +0530

    fix

--- 6-module2.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var _5_module1_1 = require("./5-module1");
-console.log(_5_module1_1.x);
-var _5_module1_2 = require("./5-module1");
-(0, _5_module1_2.myFunc)();
-var c1 = new _5_module1_2.myClass(2, 3);
-console.log(c1.add());
--- variables.js
+++
@@ -1,9 +0,0 @@
-//string
-var name1 = "Amit";
-console.log(name1);
-// number - here TypeScript will itself infered it as number so no need to explicitly assign number type
-var userId = 12309.5;
-console.log(userId.toFixed());
-// boolean
-var isAdmit = true;
-console.log(isAdmit.valueOf());
